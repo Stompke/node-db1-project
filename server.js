6578a745733d462f5aacaa94e1600935e43b2c5e
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-02-17 14:13:02 -0800

    Server API has full CRUD functionality.

--- server.js
+++ server.js
@@ -6,4 +6,58 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'you made it!' });
+})
+
+server.get('/api/accounts', (req, res) => {
+    db('accounts')
+    .then(accounts => {
+        res.status(200).json(accounts);
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'Failed to get from accounts' });
+    })
+})
+
+server.get('/api/accounts/:id', (req, res) => {
+    db.select('*').from('accounts').where({ id: req.params.id})
+    .then(account => {
+        res.status(200).json(account);
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'Failed to get from accounts' });
+    })
+})
+
+server.post('/api/accounts', (req, res) => {
+    db('accounts').insert(req.body, 'id')
+    .then(account => {
+        res.status(201).json(account);
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'Could not add account' });
+    })
+})
+
+server.put('/api/accounts/:id', (req, res) => {
+    db('accounts').where({ id: req.params.id }).update(req.body)
+    .then(account => {
+        res.status(201).json(account);
+    })
+    .catch(err => {
+        res.status(500).json({ error: "There was an error updating that account." })
+    })
+})
+
+server.delete('/api/accounts/:id', (req, res) => {
+    db('accounts').where({ id: req.params.id }).del()
+    .then(account => {
+        res.status(200).json(account);
+    })
+    .catch(err => {
+        res.status(500).json({ error: "There was an error deleting that account." });
+    })
+})
+
 module.exports = server;
